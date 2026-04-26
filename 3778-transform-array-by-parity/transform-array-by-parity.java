79e77d03cf2fc2fbb5a87cf36530192ab055baaa
Sakshisree25 <Sakshishreesm@gmail.com>
2026-04-26 10:21:04 +0530

    Time: 1 ms (100.00%) | Memory: 46.8 MB (75.60%) - LeetSync

--- 3778-transform-array-by-parity/transform-array-by-parity.java
+++ 3778-transform-array-by-parity/transform-array-by-parity.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] transformArray(int[] nums) {
-        int[]a=new int[nums.length];
+       /* int[]a=new int[nums.length];
         for(int i=0;i<nums.length;i++)
         {
             if(nums[i]%2==0)
@@ -13,7 +13,23 @@ class Solution {
             }
         }
         Arrays.sort(a);
-        return a;
+        return a;*/
+        int n=nums.length;
+        int count=0;
+        int[] ans=new int[n];
+        for(int i=0;i<n;i++){
+            if(nums[i]%2==0){
+                count++;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(i<count){
+                ans[i]=0;
+            }else{
+                ans[i]=1;
+            }
+        }
+        return ans;
 
     }
 }
