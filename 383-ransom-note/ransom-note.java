20a85c9b1671dce0529e810f7d3dbf4b04da6803
Sakshisree25 <Sakshishreesm@gmail.com>
2026-05-14 19:37:38 +0530

    Time: 17 ms (12.66%) | Memory: 47.1 MB (18.81%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+     HashMap<Character,Integer>m=new HashMap<>();
+     for(int i=0;i<magazine.length();i++)
+     {
+        m.put(magazine.charAt(i),m.getOrDefault(magazine.charAt(i),0)+1);
+     }
+     for(int i=0;i<ransomNote.length();i++)
+     {
+        if(m.containsKey(ransomNote.charAt(i)) && m.get(ransomNote.charAt(i))>0)
+        {
+            m.put(ransomNote.charAt(i),m.get(ransomNote.charAt(i))-1);
+        }
+        else
+        {
+            return false;
+        }
+     }
+   return true;
+    }
+}
