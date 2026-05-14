a1770b790a6d82bb700ceb32c89540653c4a0043
Sakshisree25 <Sakshishreesm@gmail.com>
2026-05-14 19:41:25 +0530

    Time: 1 ms (99.90%) | Memory: 46.1 MB (94.61%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-     HashMap<Character,Integer>m=new HashMap<>();
+    /* HashMap<Character,Integer>m=new HashMap<>();
      for(int i=0;i<magazine.length();i++)
      {
         m.put(magazine.charAt(i),m.getOrDefault(magazine.charAt(i),0)+1);
@@ -16,6 +16,21 @@ class Solution {
             return false;
         }
      }
-   return true;
+   return true;*/
+    int count[]=new int[26];
+    for(char ch:magazine.toCharArray())
+    {
+        count[ch-'a']++;
+    }
+    for(char ch:ransomNote.toCharArray())
+    {
+        count[ch-'a']--;
+        if(count[ch-'a']<0)
+        {
+            return false;
+        }
+    }
+    return true;
+
     }
 }
