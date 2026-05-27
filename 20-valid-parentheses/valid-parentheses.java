7d4eead4ef12cedda497708c14455a06ff3859a3
Sakshisree25 <Sakshishreesm@gmail.com>
2026-05-27 14:00:41 +0530

    Time: 4 ms (39.62%) | Memory: 43.7 MB (9.91%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,50 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()<=1)
+        {
+            return false;
+        }
+        Stack<Character>st=new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='[')
+            {
+                st.push(s.charAt(i));
+            }
+            else if(st.empty())
+            {
+                return false;
+            }
+            else
+            {
+                if(s.charAt(i)==')')
+                {
+                    if(st.pop()!='(')
+                    {
+                        return false;
+                    }
+                }
+                if(s.charAt(i)=='}')
+                {
+                    if(st.pop()!='{')
+                    {
+                        return false;
+                    }
+                }
+                if(s.charAt(i)==']')
+                {
+                    if(st.pop()!='[')
+                    {
+                        return false;
+                    }
+                }
+
+            }
+        }
+        if(st.empty())
+        {
+            return true;
+        }
+        return false;
+    }
+}
